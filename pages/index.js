31501b1362ebcb4cbedb01a8a9b1b07a1248808d
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-11-30 12:56:33 +0100

    feat(init): init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-gray-900 h-screen w-full grid place-items-center">
+        <p className="text-3xl font-mono w-fit tracking-widest text-white">
+          I just centered a div with Tailwind CSS
+        </p>
+      </div>
+    </>
+  );
+}
